cb4444dcc76c61baf06376fed40a45b5c6220fd8
Chen <Samuel.Chen33@T-Mobile.com>
2021-02-08 21:53:14 -0600

    Text changes

--- src/main/java/com/tmobile/springboot/preassessmentdemo/rest/DemoRestController.java
+++ src/main/java/com/tmobile/springboot/preassessmentdemo/rest/DemoRestController.java
@@ -10,6 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoRestController {
 	
+	@GetMapping("/")
+	public String hello() {
+		return "Hello user! Please enter an integer as path variable to add 5 to your input. \n"
+				+ "Example: https://bootcamp-preassessment.herokuapp.com/10";
+	}
+	
 	//Handles GET requests for an int parameter
 	@GetMapping("/{num}")
 	public HashMap<String, Integer> addFive(@PathVariable int num) {
