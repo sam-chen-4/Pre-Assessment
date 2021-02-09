d7a8dc3508944255fe669292ee00a555b07b9121
Chen <Samuel.Chen33@T-Mobile.com>
2021-02-08 20:29:33 -0600

    First commit

---
+++ src/main/java/com/tmobile/springboot/preassessmentdemo/PreassessmentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tmobile.springboot.preassessmentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreassessmentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreassessmentdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmobile/springboot/preassessmentdemo/PreassessmentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmobile.springboot.preassessmentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreassessmentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
