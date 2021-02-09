8988e06dca7f171e597630ce4a468fd8f632afd7
Chen <Samuel.Chen33@T-Mobile.com>
2021-02-08 21:34:47 -0600

    Changed annotation to @GetMapping and functionality to addFive

--- src/main/java/com/tmobile/springboot/preassessmentdemo/rest/DemoRestController.java
+++ src/main/java/com/tmobile/springboot/preassessmentdemo/rest/DemoRestController.java
@@ -2,6 +2,7 @@ package com.tmobile.springboot.preassessmentdemo.rest;
 
 import java.util.HashMap;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,10 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoRestController {
 	
-	@RequestMapping("/{num}")
-	public HashMap<String, Integer> addTen(@PathVariable int num) {
+	//Handles GET requests for an int parameter
+	@GetMapping("/{num}")
+	public HashMap<String, Integer> addFive(@PathVariable int num) {
+		
+		//Create new POJO
 		HashMap<String, Integer> map = new HashMap<>();
-		map.put("Sum", 10 + num);
+		
+		//Assign a key-value pair (add 5 to value)
+		map.put("sum", 5 + num);
+		
+		//Maven dependency Spring Boot Web includes Jackson - returns JSON object
 		return map;
 	}
 }
